563048e7a82f382ecf62443efdf81bd0a89cac35
zhang <718008775@qq.com>
2023-02-06 19:26:34 +0800

    20230203

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ByteDance_5th
+
+go 1.20
+
+require github.com/BurntSushi/toml v1.2.1
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"ByteDance_5th/config"
+)
+
+func main() {
+	readtoml.GetConnectionString()
+}
